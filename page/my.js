343309d564be5c44bbd3153a01052a7020252b31
Alpha-1004 <66864997+Alpha-1004@users.noreply.github.com>
2022-01-03 00:45:05 +0900

    feat: made home recommendation dynamic

--- page/my.js
+++ page/my.js
@@ -2,19 +2,15 @@ import { StyleSheet, ScrollView } from "react-native";
 import { Component } from "react/cjs/react.production.min";
 
 export class My extends Component {
-    render() {
-        return (
-            <ScrollView style={styles.Main}>
-                My
-            </ScrollView>
-        )
-    }
+  render() {
+    return <ScrollView style={styles.Main}>My</ScrollView>;
+  }
 }
 
 const styles = StyleSheet.create({
-    Main: {
-      flex: 1,
-      backgroundColor: '#262626',
-      height: 'calc(80%-10px)',
-    },
-  });
+  Main: {
+    flex: 1,
+    backgroundColor: "#262626",
+    height: "calc(80%-10px)",
+  },
+});
--- page/search.js
+++ page/search.js
@@ -2,19 +2,15 @@ import { StyleSheet, ScrollView } from "react-native";
 import { Component } from "react/cjs/react.production.min";
 
 export class Search extends Component {
-    render() {
-        return (
-            <ScrollView style={styles.Main}>
-                Search
-            </ScrollView>
-        )
-    }
+  render() {
+    return <ScrollView style={styles.Main}>Search</ScrollView>;
+  }
 }
 
 const styles = StyleSheet.create({
-    Main: {
-      flex: 1,
-      backgroundColor: '#262626',
-      height: 'calc(80%-10px)',
-    },
-  });
+  Main: {
+    flex: 1,
+    backgroundColor: "#262626",
+    height: "calc(80%-10px)",
+  },
+});
